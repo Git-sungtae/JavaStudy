9d5d614473e948a40d431f10b02f4be6f7584d20
Git-sungtae <stj0905@gmail.com>
2023-01-27 17:45:34 +0900

    Merge branch 'master' of https://github.com/Git-sungtae/JavaStudy.git

--- src/java_baekjoon/_10_3.java
+++ src/java_baekjoon/_10_3.java
@@ -4,14 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class _10_3 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int nk = Integer.parseInt(br.readLine().trim());
+		String[] values = br.readLine().split("\\s");
+		
+	
+		
+		
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+		String[] values2 = br2.readLine().split("\\s");
+		
 		
 		
 }
